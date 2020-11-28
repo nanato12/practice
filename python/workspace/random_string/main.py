42fdecb4ebd12c80da27c0beffb6ccac7d2fe68c
nanato12 <admin@nanato12.info>
2020-11-28 14:06:00 +0900

    Fix random_string

--- python/workspace/random_string/main.py
+++ python/workspace/random_string/main.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def random_string(count) -> str:
+def random_string(count: int) -> str:
     return "".join([random.choice(string.ascii_letters) for _ in range(count)])
 
 
