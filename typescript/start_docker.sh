74150c180217a560280bd8a1d1e25ba4882dd5a8
nanato12 <admin@nanato12.info>
2021-03-14 11:12:50 +0900

    Update start_docker.sh

--- typescript/start_docker.sh
+++ typescript/start_docker.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 docker build . -t typescript
-docker run -itd --name ts-practice -p 8888:8080 -v $PWD/workspace:/workspace --workdir /workspace typescript
+docker run -itd --name ts-practice -p 8080:8080 -v $PWD/workspace:/workspace --workdir /workspace typescript
 docker exec -it ts-practice /bin/sh
